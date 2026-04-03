402110740bf9d158a14c230d61f7d97651f4bca5
Ibbah2000 <ibrahimacaba.bah@etu.univ-tours.fr>
2026-04-03 18:06:02 +0200

    IC - Ajout de scheduler.py

---
+++ src/film_scheduler/scheduler.py
@@ -0,0 +1,28 @@
+"""Description.
+
+Calcul du planning optimal d'un projet.
+"""
+
+from src.film_scheduler.models import Projet
+
+
+def calcule_dates(projet: Projet) -> dict[str, int]:
+    """Donne la date de fin au plus tôt pour chaque tâche."""
+    fins = {}
+    taches_restantes = list(projet.taches)
+
+    while taches_restantes:
+        for tache in taches_restantes:
+            if all(p in fins for p in tache.predecesseurs):
+                debut = max((fins[p] for p in tache.predecesseurs), default=0)
+                fins[tache.nom] = debut + tache.duree
+                taches_restantes.remove(tache)
+                break
+
+    return fins
+
+
+def duree_totale(projet: Projet) -> int:
+    """Donne la durée totale du projet en jours."""
+    fins = calcule_dates(projet)
+    return max(fins.values())
